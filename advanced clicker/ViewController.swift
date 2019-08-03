f4bc3d6233583405e133d9256d142171db37303a
celeste <celiresmart@gmail.com>
2019-08-03 08:53:56 +0800

    Initial Commit

---
+++ advanced clicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  advanced clicker
+//
+//  Created by Celeste Tan on 3/8/19.
+//  Copyright © 2019 cuzwhynot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
